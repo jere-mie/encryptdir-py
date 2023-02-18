ddccc3a8936214a108432b822c7029e321d71265
jere-mie <jeremiedevelops@gmail.com>
2023-02-18 00:20:22 -0500

    implemented signing aes keychain

--- aes_utils.py
+++ aes_utils.py
@@ -1,5 +1,6 @@
 from Cryptodome.Random import get_random_bytes
-from rsa_utils import buffered_encrypt_with_rsa, buffered_decrypt_with_rsa
+from rsa_utils import buffered_encrypt_with_rsa, buffered_decrypt_with_rsa, sign_message, verify_signature
+import hashlib
 
 def create_aes_key(size):
     size_in_bytes = size//8  # 128 bits / 8 = 16 bytes (1 byte = 8 bits)
@@ -34,3 +35,20 @@ def read_aes_keys(file_extensions, size, rsa_privatekey, infile):
         keymap[fe] = decrypted[:size//8]
         decrypted = decrypted[size//8:]
     return keymap
+
+def sign_aes_keychain(rsa_privatekey, keychain_file):
+    with open(keychain_file, 'rb') as f:
+        data = f.read()
+    hash = hashlib.md5(data).hexdigest()
+    signature = sign_message(rsa_privatekey, hash)
+    with open(f'{keychain_file}.signature', 'wb') as f:
+        f.write(signature)
+    return signature
+
+def verify_aes_keychain_signature(rsa_publickey, keychain_file):
+    with open(keychain_file, 'rb') as f:
+        data = f.read()
+    hash = hashlib.md5(data).hexdigest()
+    with open(f'{keychain_file}.signature', 'rb') as f:
+        signature = f.read()
+    return verify_signature(rsa_publickey, hash, signature)
